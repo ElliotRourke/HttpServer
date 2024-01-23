2e605c4d661c15c17855bc3094c4e8dbe6475baa
elliot <elliot.rourke@gmail.com>
2024-01-23 22:05:12 +0000

    Fleshing out basics

--- src/main/java/org/elliot/ConnectionHandler.java
+++ src/main/java/org/elliot/ConnectionHandler.java
@@ -1,10 +1,13 @@
 package org.elliot;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class ConnectionHandler {
@@ -16,20 +19,21 @@ public class ConnectionHandler {
     }
 
     public void handleConnection() {
-            try (Socket client = serverSocket.accept()) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
-                    System.out.println("Entering");
-//                    String line;
-//                    while (( line = reader.readLine()) != null) {
-//                        System.out.println(line);
-//                    }
-                    reader
-                            .lines()
-                            .forEach(System.out::println);
-                    System.out.println("Exiting");
-                } catch (Exception e) {
-                    throw new RuntimeException("Inputstream failure!", e);
+            try (
+                    Socket client = serverSocket.accept();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream()))
+            ) {
+                String input;
+                String output;
+
+                writer.println("Welcome! You've connected to MyHttpServer!");
+
+                while((input = reader.readLine()) != null) {
+                    output = "I have received your input!";
+                    writer.println(output);
                 }
+
             } catch (Exception e) {
                 throw new RuntimeException("Client failure!", e);
             }
--- src/main/java/org/elliot/MyHttpServer.java
+++ src/main/java/org/elliot/MyHttpServer.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 
 public class MyHttpServer {
     public static void main(String[] args) throws UnknownHostException {
-        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+        try (ServerSocket serverSocket = new ServerSocket( 8080)) {
             final ConnectionHandler connectionHandler = new ConnectionHandler(serverSocket);
             while (true) {
                 connectionHandler.handleConnection();
